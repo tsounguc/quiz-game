1f8388881d2ff90d0963b8db402f3c1f91e63eeb
Christian Tsoungui NKoulou <tsounguc@mail.gvsu.edu>
2023-10-14 17:14:47 -0400

    Continue showing new questions

--- quiz_brain.py
+++ quiz_brain.py
@@ -8,4 +8,8 @@ class QuizBrain:
 
     def next_question(self):
         current_question = self.question_list[self.question_number]
-        user_answer = input(f"Q.{self.question_number + 1}: {current_question.text} (True/False)?: ")
+        self.question_number += 1
+        user_answer = input(f"Q.{self.question_number}: {current_question.text} (True/False)?: ")
+
+    def still_has_questions(self):
+        return self.question_number < len(self.question_list)
